e3b92048c704954ba48f0ded71a5a3e34ad15d4d
ape04 <an1287635@gmail.com>
2019-03-21 15:23:10 +0000

    small change

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -7,6 +7,11 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("goethe")) {
+            return "William Shakespeare goethe(26 April 1564 - 23 April 1616) was an " +
+                    "English poet, playwright, and actor, widely regarded as the greatest " +
+                    "writer in the English language and the world's pre-eminent dramatist.";
+        }
         return "";
     }
 }
